f3529ca880a8941fdb4be0e7d2abda0c4ded8d55
Hendrik Mans <hendrik@mans.de>
2022-04-22 18:23:54 +0200

    vite

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+      "miniplex",
+      "miniplex-react",
+      "solid-trinity",
+      "@hmans/controlfreak",
+      "@hmans/signal",
+      "@hmans/ui"
+    ],
+    include: ["react/jsx-runtime"]
+  },
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
-  },
-});
+    target: "esnext",
+    polyfillDynamicImport: false
+  }
+})
