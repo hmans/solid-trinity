6aa622797557a026f1273b9b17145e02afbb64c7
Hendrik Mans <hendrik@mans.de>
2022-04-22 11:24:55 +0200

    Cool?

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,127 @@
-import type { Component } from "solid-js";
+import {
+  Component,
+  createContext,
+  onCleanup,
+  splitProps,
+  useContext,
+} from "solid-js";
 import * as THREE from "three";
+import { applyProps } from "./applyProps";
 
-const ThreeGame: Component = () => {
+const onAnimationFrame = (fn: Function) => {
+  let running = true;
+
+  const animate = () => {
+    fn();
+    if (running) requestAnimationFrame(animate);
+  };
+  requestAnimationFrame(animate);
+
+  onCleanup(() => (running = false));
+};
+
+const ParentContext = createContext<any>();
+
+type THREE = typeof THREE;
+
+type Constructor<T = any> = { new (...args: any[]): T };
+
+type ThreeComponentProps<T> = any;
+
+type ThreeComponent<T> = Component<ThreeComponentProps<T>>;
+
+const makeThreeComponent =
+  <TName extends keyof THREE, T extends THREE[TName]>(
+    name: TName
+  ): ThreeComponent<T> =>
+  (props) => {
+    const [local, instanceProps] = splitProps(props, ["children"]);
+
+    /* Fetch the constructor */
+    const klass = THREE[name];
+
+    /* Create instance */
+    const instance = new klass();
+
+    /* Assign ref */
+    typeof props.ref === "function"
+      ? props.ref(instance)
+      : (props.ref = instance);
+
+    /* Connect to parent */
+    const parent = useContext(ParentContext);
+    if (
+      instance instanceof THREE.Object3D &&
+      parent instanceof THREE.Object3D
+    ) {
+      parent.add(instance);
+      onCleanup(() => parent.remove(instance));
+    }
+
+    /* Attach */
+    if (instance instanceof THREE.Material) parent.material = instance;
+    if (instance instanceof THREE.BufferGeometry) parent.geometry = instance;
+
+    /* Apply props */
+    applyProps(instance, instanceProps);
+
+    /* Automatically dispose */
+    if ("dispose" in instance) onCleanup(() => instance.dispose());
+
+    return (
+      <ParentContext.Provider value={instance}>
+        {local.children}
+      </ParentContext.Provider>
+    );
+  };
+
+const Mesh = makeThreeComponent("Mesh");
+const MeshStandardMaterial = makeThreeComponent("MeshStandardMaterial");
+const DodecahedronGeometry = makeThreeComponent("DodecahedronGeometry");
+
+const ThreeGame: Component = (props) => {
   const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  const scene = new THREE.Scene();
 
-  return renderer.domElement;
+  scene.add(new THREE.AmbientLight());
+  scene.add(new THREE.DirectionalLight());
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight
+  );
+  camera.position.z = 10;
+
+  onAnimationFrame(() => {
+    renderer.render(scene, camera);
+  });
+
+  return (
+    <>
+      {renderer.domElement}
+      <ParentContext.Provider value={scene}>
+        {props.children}
+      </ParentContext.Provider>
+    </>
+  );
 };
 
 const App: Component = () => {
-  return <ThreeGame />;
+  let mesh: THREE.Mesh | null = null;
+
+  onAnimationFrame(() => {
+    mesh!.rotation.x = mesh!.rotation.y += 0.01;
+  });
+
+  return (
+    <ThreeGame>
+      <Mesh ref={mesh}>
+        <DodecahedronGeometry />
+        <MeshStandardMaterial color="hotpink" />
+      </Mesh>
+    </ThreeGame>
+  );
 };
 
 export default App;
