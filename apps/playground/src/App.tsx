9793f497382bfffdb7c397084e79903a44d5f2f7
Hendrik Mans <hendrik@mans.de>
2022-04-22 17:28:17 +0200

    Alright let's go

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,16 +1,15 @@
-import { Component } from "solid-js"
-import * as THREE from "three"
-import T, { onAnimationFrame, ParentContext } from "./solid-trinity"
+import { Component, createSignal } from "solid-js"
+import T, { onAnimationFrame, Trinity } from "solid-trinity"
 
 const Thingy = () => {
-  let mesh: THREE.Mesh = null!
+  const [rot, setRot] = createSignal(0)
 
   onAnimationFrame(() => {
-    mesh.rotation.x = mesh.rotation.y += 0.01
+    setRot((rot) => (rot += 0.01))
   })
 
   return (
-    <T.Mesh ref={mesh} scale={1}>
+    <T.Mesh rotation-z={rot()} scale={1}>
       <T.DodecahedronGeometry />
       <T.MeshStandardMaterial color="hotpink" />
     </T.Mesh>
@@ -18,11 +17,11 @@ const Thingy = () => {
 }
 
 const App: Component = () => (
-  <ThreeGame>
+  <Trinity>
     <T.AmbientLight intensity={0.2} />
     <T.DirectionalLight position={[10, 10, 10]} intensity={0.6} />
     <Thingy />
-  </ThreeGame>
+  </Trinity>
 )
 
 export default App
--- packages/solid-trinity/src/makeThreeComponent.tsx
+++ packages/solid-trinity/src/makeThreeComponent.tsx
@@ -1,18 +1,19 @@
 import {
   Component,
   createContext,
+  createEffect,
   onCleanup,
   splitProps,
-  useContext,
-} from "solid-js";
-import * as THREE from "three";
-import { applyProps } from "./applyProps";
+  useContext
+} from "solid-js"
+import * as THREE from "three"
+import { applyProps } from "./applyProps"
 
-export const ParentContext = createContext<any>();
+export const ParentContext = createContext<any>()
 
-type THREE = typeof THREE;
+type THREE = typeof THREE
 
-export type Constructor<Instance = any> = { new (...args: any[]): Instance };
+export type Constructor<Instance = any> = { new (...args: any[]): Instance }
 
 export type ThreeComponentProps<
   Klass extends Constructor<any>,
@@ -21,36 +22,36 @@ export type ThreeComponentProps<
   RefProp<Instance> &
   AttachProp &
   ConstructorArgsProps<Klass> &
-  ObjectProp<Instance>;
+  ObjectProp<Instance>
 
-type MainProps<T> = Omit<ConvenienceProps<T>, "children" | "attach" | "args">;
+type MainProps<T> = Omit<ConvenienceProps<T>, "children" | "attach" | "args">
 
 type ConvenienceProps<T> = {
-  [K in keyof T]?: SetArgumentType<T, K> | SetScalarArgumentType<T, K>;
-};
+  [K in keyof T]?: SetArgumentType<T, K> | SetScalarArgumentType<T, K>
+}
 
 type SetArgumentType<T, K extends keyof T> = T[K] extends {
-  set: (...args: infer Arguments) => any;
+  set: (...args: infer Arguments) => any
 }
   ? Arguments extends [any]
     ? Arguments[0] | T[K]
     : Arguments | T[K]
-  : T[K];
+  : T[K]
 
 type SetScalarArgumentType<T, K extends keyof T> = T[K] extends {
-  setScalar: (scalar: infer Argument) => any;
+  setScalar: (scalar: infer Argument) => any
 }
   ? Argument | T[K]
-  : T[K];
+  : T[K]
 
 type AttachProp = {
   /** Attach the object to the parent property specified here. */
-  attach?: string;
-};
+  attach?: string
+}
 
-type Ref<T> = { (val: T): void } | T;
+type Ref<T> = { (val: T): void } | T
 
-type RefProp<T> = { ref?: Ref<T> };
+type RefProp<T> = { ref?: Ref<T> }
 
 /**
  * Our wrapper components allow the user to pass an already instantiated object, or it will create a new
@@ -58,82 +59,80 @@ type RefProp<T> = { ref?: Ref<T> };
  */
 type ObjectProp<T> = {
   /** If you already have an instance of the class you want to wrap, you can pass it here. */
-  object?: T | { dispose?: () => void };
-};
+  object?: T | { dispose?: () => void }
+}
 
 /** Some extra props we will be accepting on our wrapper component. */
 type ConstructorArgsProps<TConstructor extends Constructor> = {
   /** Arguments passed to the wrapped THREE class' constructor. */
   // args?: TConstructor extends new (...args: infer V) => any ? V : never;
-  args?: ConstructorParameters<TConstructor>;
-};
+  args?: ConstructorParameters<TConstructor>
+}
 
 export type ThreeComponent<
   Klass extends Constructor,
   Instance = InstanceType<Klass>
-> = Component<ThreeComponentProps<Klass, Instance>>;
+> = Component<ThreeComponentProps<Klass, Instance>>
 
-export const makeThreeComponent =
-  <Klass extends Constructor, Instance = InstanceType<Klass>>(
-    klass: Klass
-  ): ThreeComponent<Klass, Instance> =>
-  (props) => {
-    const [local, instanceProps] = splitProps(props, [
-      "ref",
-      "args",
-      "attach",
-      "children",
-    ]);
+export const makeThreeComponent = <
+  Klass extends Constructor,
+  Instance = InstanceType<Klass>
+>(
+  klass: Klass
+): ThreeComponent<Klass, Instance> => (props) => {
+  const [local, instanceProps] = splitProps(props, [
+    "ref",
+    "args",
+    "attach",
+    "children"
+  ])
 
-    const parent = useContext(ParentContext);
+  const parent = useContext(ParentContext)
 
-    /* Create instance */
-    const instance = new klass(...(local.args ?? [])) as Instance;
+  /* Create instance */
+  const instance = new klass(...(local.args ?? [])) as Instance
 
-    /* Assign ref */
-    if ("ref" in props)
-      typeof props.ref !== "function"
-        ? (props.ref = instance)
-        : (props.ref as Function)(instance);
+  /* Assign ref */
+  if ("ref" in props)
+    typeof props.ref !== "function"
+      ? (props.ref = instance)
+      : (props.ref as Function)(instance)
 
-    /* Apply props */
-    applyProps(instance, instanceProps);
+  /* Apply props */
+  createEffect(() => applyProps(instance, instanceProps))
 
-    /* Connect to parent */
-    if (
-      instance instanceof THREE.Object3D &&
-      parent instanceof THREE.Object3D
-    ) {
-      parent.add(instance);
-      onCleanup(() => parent.remove(instance));
+  /* Connect to parent */
+  if (instance instanceof THREE.Object3D && parent instanceof THREE.Object3D) {
+    parent.add(instance)
+    onCleanup(() => parent.remove(instance))
+  }
+
+  /* Attach */
+  let attach: string | undefined = local.attach
+  if (!attach) {
+    if (instance instanceof THREE.Material) attach = "material"
+    else if (instance instanceof THREE.BufferGeometry) attach = "geometry"
+    else if (instance instanceof THREE.Fog) attach = "fog"
+  }
+
+  /* If the instance has an "attach" property, attach it to the parent */
+  if (attach) {
+    if (attach in parent) {
+      parent[attach] = instance
+      onCleanup(() => void (parent[attach!] = undefined))
+    } else {
+      console.error(
+        `Property "${attach}" does not exist on parent "${parent.constructor.name}"`
+      )
     }
+  }
 
-    /* Attach */
-    let attach: string | undefined = local.attach;
-    if (!attach) {
-      if (instance instanceof THREE.Material) attach = "material";
-      else if (instance instanceof THREE.BufferGeometry) attach = "geometry";
-      else if (instance instanceof THREE.Fog) attach = "fog";
-    }
+  /* Automatically dispose */
+  if ("dispose" in instance) onCleanup(() => (instance as any).dispose())
 
-    /* If the instance has an "attach" property, attach it to the parent */
-    if (attach) {
-      if (attach in parent) {
-        parent[attach] = instance;
-        onCleanup(() => void (parent[attach!] = undefined));
-      } else {
-        console.error(
-          `Property "${attach}" does not exist on parent "${parent.constructor.name}"`
-        );
-      }
-    }
-
-    /* Automatically dispose */
-    if ("dispose" in instance) onCleanup(() => (instance as any).dispose());
-
-    return (
-      <ParentContext.Provider value={instance}>
-        {local.children}
-      </ParentContext.Provider>
-    );
-  };
+  return (
+    <ParentContext.Provider value={instance}>
+      {local.children}
+    </ParentContext.Provider>
+  )
+}
