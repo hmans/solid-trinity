474681c06f3ff025528a62aac38a1721ef39557d
Hendrik Mans <hendrik@mans.de>
2022-04-22 11:51:18 +0200

    Split things up

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,7 @@
-import {
-  Component,
-  createContext,
-  onCleanup,
-  splitProps,
-  useContext,
-} from "solid-js";
+import { Component } from "solid-js";
 import * as THREE from "three";
-import { applyProps } from "./applyProps";
-
-const onAnimationFrame = (fn: Function) => {
-  let running = true;
-
-  const animate = () => {
-    fn();
-    if (running) requestAnimationFrame(animate);
-  };
-  requestAnimationFrame(animate);
-
-  onCleanup(() => (running = false));
-};
-
-const ParentContext = createContext<any>();
-
-type THREE = typeof THREE;
-
-type ConstructibleTHREE = {
-  [K in keyof THREE]: THREE[K] extends Constructor ? THREE[K] : never;
-};
-
-type Constructor<Instance = any> = { new (...args: any[]): Instance };
-
-type ThreeComponentProps<Instance> = any;
-
-type ThreeComponent<Instance> = Component<ThreeComponentProps<Instance>>;
-
-const makeThreeComponent =
-  <TName extends keyof ConstructibleTHREE, Klass = ConstructibleTHREE[TName]>(
-    name: TName
-  ): ThreeComponent<Klass> =>
-  (props) => {
-    const [local, instanceProps] = splitProps(props, ["children"]);
-
-    /* Fetch the constructor */
-    const klass = THREE[name] as ConstructibleTHREE[TName];
-
-    /* Create instance */
-    const instance = new klass();
-
-    /* Assign ref */
-    typeof props.ref === "function"
-      ? props.ref(instance)
-      : (props.ref = instance);
-
-    /* Connect to parent */
-    const parent = useContext(ParentContext);
-    if (
-      instance instanceof THREE.Object3D &&
-      parent instanceof THREE.Object3D
-    ) {
-      parent.add(instance);
-      onCleanup(() => parent.remove(instance));
-    }
-
-    /* Attach */
-    if (instance instanceof THREE.Material) parent.material = instance;
-    if (instance instanceof THREE.BufferGeometry) parent.geometry = instance;
-
-    /* Apply props */
-    applyProps(instance, instanceProps);
-
-    /* Automatically dispose */
-    if ("dispose" in instance) onCleanup(() => instance.dispose());
-
-    return (
-      <ParentContext.Provider value={instance}>
-        {local.children}
-      </ParentContext.Provider>
-    );
-  };
-
-const T = {
-  Mesh: makeThreeComponent("Mesh"),
-  MeshStandardMaterial: makeThreeComponent("MeshStandardMaterial"),
-  DodecahedronGeometry: makeThreeComponent("DodecahedronGeometry"),
-};
+import { onAnimationFrame } from "./onAnimationFrame";
+import T, { ParentContext } from "./Reactor";
 
 const ThreeGame: Component = (props) => {
   const renderer = new THREE.WebGLRenderer();
@@ -113,7 +31,7 @@ const ThreeGame: Component = (props) => {
   );
 };
 
-const App: Component = () => {
+const Thingy = () => {
   let mesh: THREE.Mesh | null = null;
 
   onAnimationFrame(() => {
@@ -121,13 +39,17 @@ const App: Component = () => {
   });
 
   return (
-    <ThreeGame>
-      <T.Mesh ref={mesh} scale={2}>
-        <T.DodecahedronGeometry />
-        <T.MeshStandardMaterial color="hotpink" />
-      </T.Mesh>
-    </ThreeGame>
+    <T.Mesh ref={mesh} scale={2}>
+      <T.DodecahedronGeometry />
+      <T.MeshStandardMaterial color="hotpink" />
+    </T.Mesh>
   );
 };
+
+const App: Component = () => (
+  <ThreeGame>
+    <Thingy />
+  </ThreeGame>
+);
 
 export default App;
---
+++ src/Reactor.tsx
@@ -0,0 +1,76 @@
+import {
+  Component,
+  createContext,
+  onCleanup,
+  splitProps,
+  useContext,
+} from "solid-js";
+import * as THREE from "three";
+import { applyProps } from "./applyProps";
+
+export const ParentContext = createContext<any>();
+
+type THREE = typeof THREE;
+
+type ConstructibleTHREE = {
+  [K in keyof THREE]: THREE[K] extends Constructor ? THREE[K] : never;
+};
+
+type Constructor<Instance = any> = { new (...args: any[]): Instance };
+
+type ThreeComponentProps<Instance> = any;
+
+type ThreeComponent<Instance> = Component<ThreeComponentProps<Instance>>;
+
+export const makeThreeComponent =
+  <TName extends keyof ConstructibleTHREE, Klass = ConstructibleTHREE[TName]>(
+    name: TName
+  ): ThreeComponent<Klass> =>
+  (props) => {
+    const [local, instanceProps] = splitProps(props, ["children"]);
+
+    /* Fetch the constructor */
+    const klass = THREE[name] as ConstructibleTHREE[TName];
+
+    /* Create instance */
+    const instance = new klass();
+
+    /* Assign ref */
+    typeof props.ref === "function"
+      ? props.ref(instance)
+      : (props.ref = instance);
+
+    /* Connect to parent */
+    const parent = useContext(ParentContext);
+    if (
+      instance instanceof THREE.Object3D &&
+      parent instanceof THREE.Object3D
+    ) {
+      parent.add(instance);
+      onCleanup(() => parent.remove(instance));
+    }
+
+    /* Attach */
+    if (instance instanceof THREE.Material) parent.material = instance;
+    if (instance instanceof THREE.BufferGeometry) parent.geometry = instance;
+
+    /* Apply props */
+    applyProps(instance, instanceProps);
+
+    /* Automatically dispose */
+    if ("dispose" in instance) onCleanup(() => instance.dispose());
+
+    return (
+      <ParentContext.Provider value={instance}>
+        {local.children}
+      </ParentContext.Provider>
+    );
+  };
+
+const T = {
+  Mesh: makeThreeComponent("Mesh"),
+  MeshStandardMaterial: makeThreeComponent("MeshStandardMaterial"),
+  DodecahedronGeometry: makeThreeComponent("DodecahedronGeometry"),
+};
+
+export default T;
